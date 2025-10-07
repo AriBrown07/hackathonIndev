21f38389ba385cac5ec6694b70267b060d86b356
arinakapkovi4 <arinakapkovi4@gmail.com>
2025-10-07 12:26:07 +0300

    Authorization

--- Hackathon.UI/src/pages/Authorization/Authorization.tsx
+++ Hackathon.UI/src/pages/Authorization/Authorization.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from './lib/supabase';
+import supabase from "../../utils/supabase";
 import { LogIn, UserPlus, Mail, Lock, Eye, EyeOff } from 'lucide-react';
 import styles from './Authorization.module.scss';
 
--- Hackathon.UI/src/routes/AppRoutes.tsx
+++ Hackathon.UI/src/routes/AppRoutes.tsx
@@ -3,6 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage/HomePage';
 import FacePage from '../pages/FacePage/FacePage';
+import Diagnostics from '../pages/Diagnostics/Diagnostics';
+import Authorization from '../pages/Authorization/Authorization';
+
+const handleAuthSuccess = () => {
+    console.log("Авторизация прошла успешно!");
+    // например, редирект на другую страницу:
+    // navigate("/dashboard");
+  };
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -11,6 +19,8 @@ export const AppRoutes: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/main" element={<FacePage />} />
+        <Route path="/Diagnostics" element={<Diagnostics />} />
+        <Route path="/Authorization" element={<Authorization onAuthSuccess={handleAuthSuccess}  />} />
 
     </Routes>
   );
